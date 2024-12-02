ea502bd8f0925cd3a3d75fe5660e7e1abb2bcdb3
SunshineAlis <bumaam@yahoo.com>
2024-12-02 18:14:28 +0800

    formula

---
+++ index.js
@@ -0,0 +1,28 @@
+console.log("test");
+
+const number = 1;
+const stringnumber = "1";
+console.log(number === stringnumber);
+console.log(number == stringnumber);
+// gurvaljingin perimetr oloh
+let a = 10;
+let b = 8;
+let c = 14;
+const hariu = a + b + c;
+console.log(hariu);
+// const hariu=gurvaljingin perimetr; console.log(a + b + c);
+const d = 2;
+console.log(2 * 3);
+const pr = 3.14;
+const radius = 3;
+const togtmol = 2;
+console.log(pr * radius * togtmol);
+const y = 2;
+const x = 3;
+const m = y * 2 - ((y * 1) / x) * 2 - x * 1;
+console.log(m);
+// Bit rate = (Sample Size) x (Number of Samples per Second) x (Number of Channels)
+const samplesize = 15;
+const persecond = 3;
+const numberofchannel = 1;
+console.log(samplesize * persecond * numberofchannel);
